3f001244617a9a47fdd7958b2541d7c60439524d
dcyrillo <d.cyrillo@hotmail.com>
2019-08-25 19:00:49 -0300

    Add the Rest Standart for Category End Point

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MovieModule } from './Movie/movie.module';
+import { MovieModule } from './movie/movie.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig),MovieModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig),MovieModule, CategoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
