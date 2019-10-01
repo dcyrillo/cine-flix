592cc738a9dfc27449958c641fa905cd8cab96e3
dcyrillo <d.cyrillo@hotmail.com>
2019-10-01 09:33:47 -0300

    Cloud Cine Flix 1.0.0

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
@@ -14,10 +15,12 @@ async function bootstrap() {
     .addTag('categories')
     .build();
   dotenvFlow.config();
-
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(process.env.PORT || 3000);
+
+  await app.listen(app.get('ConfigService').get('express.port'));
 }
 bootstrap();
